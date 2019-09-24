8328c96e55e65cdeef63f69769dca3fb948e6bb0
Theis Bazin <theis.bazin@outlook.com>
2019-09-24 14:28:06 +0200

    :bug: Use decay value from arguments

--- vqvae.py
+++ vqvae.py
@@ -170,12 +170,12 @@ class VQVAE(nn.Module):
         self.enc_b = Encoder(in_channel, channel, n_res_block, n_res_channel, stride=4)
         self.enc_t = Encoder(channel, channel, n_res_block, n_res_channel, stride=2)
         self.quantize_conv_t = nn.Conv2d(channel, embed_dim, 1)
-        self.quantize_t = Quantize(embed_dim, n_embed)
+        self.quantize_t = Quantize(embed_dim, n_embed, decay=decay)
         self.dec_t = Decoder(
             embed_dim, embed_dim, channel, n_res_block, n_res_channel, stride=2
         )
         self.quantize_conv_b = nn.Conv2d(embed_dim + channel, embed_dim, 1)
-        self.quantize_b = Quantize(embed_dim, n_embed)
+        self.quantize_b = Quantize(embed_dim, n_embed, decay=decay)
         self.upsample_t = nn.ConvTranspose2d(
             embed_dim, embed_dim, 4, stride=2, padding=1
         )
