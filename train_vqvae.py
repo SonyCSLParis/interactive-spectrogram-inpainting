49153605dca9da0b95db274fd79718dac73f3623
Theis Bazin <theis.bazin@outlook.com>
2019-09-24 14:38:54 +0200

    :sparkles: Add support for NSynth data

--- train_vqvae.py
+++ train_vqvae.py
@@ -1,4 +1,5 @@
 import argparse
+import pathlib
 
 import torch
 from torch import nn, optim
@@ -11,6 +12,10 @@ from tqdm import tqdm
 from vqvae import VQVAE
 from scheduler import CycleScheduler
 
+from nsynth_dataset import NSynthDataset
+
+import os
+DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
 def train(epoch, loader, model, optimizer, scheduler, device):
     loader = tqdm(loader)
@@ -18,12 +23,12 @@ def train(epoch, loader, model, optimizer, scheduler, device):
     criterion = nn.MSELoss()
 
     latent_loss_weight = 0.25
-    sample_size = 25
+    image_dump_sample_size = 25
 
     mse_sum = 0
     mse_n = 0
 
-    for i, (img, label) in enumerate(loader):
+    for i, ((img, pitch), target) in enumerate(loader):
         model.zero_grad()
 
         img = img.to(device)
@@ -54,18 +59,24 @@ def train(epoch, loader, model, optimizer, scheduler, device):
         if i % 100 == 0:
             model.eval()
 
-            sample = img[:sample_size]
+            sample = img[:image_dump_sample_size]
 
             with torch.no_grad():
                 out, _ = model(sample)
 
-            utils.save_image(
-                torch.cat([sample, out], 0),
-                f'sample/{str(epoch + 1).zfill(5)}_{str(i).zfill(5)}.png',
-                nrow=sample_size,
-                normalize=True,
-                range=(-1, 1),
-            )
+            channel_dim = 1
+            for channel_index, channel_name in enumerate(['spectrogram', 'instantaneous_frequency']):
+                sample_channel = sample.select(channel_dim, channel_index).unsqueeze(channel_dim)
+                out_channel = out.select(channel_dim, channel_index).unsqueeze(channel_dim)
+                utils.save_image(
+                    torch.cat([sample_channel, out_channel], 0),
+                    os.path.join(DIRPATH, 'sample/',
+                                 f'{str(epoch + 1).zfill(5)}_{str(i).zfill(5)}_{channel_name}.png'),
+                    nrow=image_dump_sample_size,
+                    # normalize=True,
+                    # range=(-1, 1),
+                    # scale_each=True,
+                )
 
             model.train()
 
@@ -75,8 +86,11 @@ if __name__ == '__main__':
     parser.add_argument('--size', type=int, default=256)
     parser.add_argument('--epoch', type=int, default=560)
     parser.add_argument('--lr', type=float, default=3e-4)
+    parser.add_argument('--dataset', type=str)
     parser.add_argument('--sched', type=str)
-    parser.add_argument('path', type=str)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--num_workers', type=int, default=4)
+    parser.add_argument('--path', type=str)
 
     args = parser.parse_args()
 
@@ -84,19 +98,51 @@ if __name__ == '__main__':
 
     device = 'cuda'
 
-    transform = transforms.Compose(
-        [
-            transforms.Resize(args.size),
-            transforms.CenterCrop(args.size),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
-        ]
-    )
+    path = pathlib.Path(args.path)
+    dataset_name = args.dataset
+    print("Loading dataset: ", dataset_name)
+    vqvae_decoder_activation = None
+    if dataset_name == 'imagenet':
+        transform = transforms.Compose(
+            [
+                transforms.Resize(args.size),
+                transforms.CenterCrop(args.size),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+            ]
+        )
+        dataset = datasets.ImageFolder(args.path, transform=transform)
+        loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+        dataloader_for_gansynth_normalization = None
+        in_channel = 3
+    elif dataset_name == 'nsynth':
+        nsynth_dataset_path = args.path
+        nsynth_dataset = NSynthDataset(
+            root_path=nsynth_dataset_path,
+            use_mel_frequency_scale=True)
+        loader = DataLoader(nsynth_dataset, batch_size=args.batch_size,
+                            num_workers=args.num_workers, shuffle=True)
+        
+        in_channel = 2
+        
+        # vqvae_decoder_activation = nn.Tanh()
+        dataloader_for_gansynth_normalization = loader
+    else:
+        raise ValueError("Unrecognized dataset name: ",
+                         dataset_name)
 
-    dataset = datasets.ImageFolder(args.path, transform=transform)
-    loader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=4)
-
-    model = nn.DataParallel(VQVAE()).to(device)
+    print("Initializing model")
+    vqvae = VQVAE(in_channel=in_channel,
+                  decoder_output_activation=vqvae_decoder_activation,
+                  dataloader_for_gansynth_normalization=dataloader_for_gansynth_normalization
+                  )
+    if dataloader_for_gansynth_normalization is not None:
+        # store normalization parameters
+        data_normalizer = vqvae.data_normalizer
+        normalization_statistics_path = path / '../normalization_statistics.pkl'
+        data_normalizer.dump_statistics(normalization_statistics_path)
+    
+    model = nn.DataParallel(vqvae).to(device)
 
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     scheduler = None
@@ -105,8 +151,9 @@ if __name__ == '__main__':
             optimizer, args.lr, n_iter=len(loader) * args.epoch, momentum=None
         )
 
+    print("Starting training")
     for i in range(args.epoch):
         train(i, loader, model, optimizer, scheduler, device)
         torch.save(
-            model.module.state_dict(), f'checkpoint/vqvae_{str(i + 1).zfill(3)}.pt'
+            model.module.state_dict(), f'checkpoint/vqvae_{dataset_name}_{str(i + 1).zfill(3)}.pt'
         )
