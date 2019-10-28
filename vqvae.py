aade60a20d865e3255efacb76eb56f3bc5612766
Theis Bazin <theis.bazin@outlook.com>
2019-10-28 15:55:21 +0100

    :sparkles: Code variance control + code corruption

--- vqvae.py
+++ vqvae.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Iterable, Union, List, Mapping
 import numpy as np
 
 import torch
@@ -29,15 +29,21 @@ from GANsynth_pytorch.normalizer import DataNormalizer
 
 
 class Quantize(nn.Module):
-    def __init__(self, dim, n_embed, decay=0.99, eps=1e-5):
+    def __init__(self, dim: int, n_embed: int, decay: float = 0.99,
+                 eps: float = 1e-5, embeddings_initial_variance: float = 1,
+                 corruption_weights: Optional[List[float]] = None):
         super().__init__()
 
         self.dim = dim
         self.n_embed = n_embed
         self.decay = decay
         self.eps = eps
+        self.corruption_weights = corruption_weights
+        self.embeddings_initial_variance = embeddings_initial_variance
 
-        embed = torch.randn(dim, n_embed)
+        # initialize embeddings
+        embed = (torch.randn(dim, n_embed)
+                 * np.sqrt(self.embeddings_initial_variance))
         self.register_buffer('embed', embed)
         self.register_buffer('cluster_size', torch.zeros(n_embed))
         self.register_buffer('embed_avg', embed.clone())
@@ -50,6 +56,19 @@ class Quantize(nn.Module):
             + self.embed.pow(2).sum(0, keepdim=True)
         )
         _, embed_ind = (-dist).max(1)
+
+        if self.training and self.corruption_weights is not None:
+            num_indexes = embed_ind.numel()
+            random_plus_minus_one = (
+                (torch.multinomial(torch.Tensor(self.corruption_weights),
+                                   num_indexes,
+                                   replacement=True)
+                 - 1)
+                .reshape(embed_ind.shape)
+                .to(embed_ind.device)
+            )
+            embed_ind = (embed_ind + random_plus_minus_one) % self.n_embed
+
         embed_onehot = F.one_hot(embed_ind, self.n_embed).type(flatten.dtype)
         embed_ind = embed_ind.view(*input.shape[:-1])
         quantize = self.embed_code(embed_ind)
