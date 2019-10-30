d86ff540af0ba940a9a4568babe6756c502eb927
Theis Bazin <theis.bazin@outlook.com>
2019-10-30 16:20:38 +0100

    :sparkles: Adapt to NSynth data

--- extract_code.py
+++ extract_code.py
@@ -1,7 +1,11 @@
 import argparse
 import pickle
+import json
+import pathlib
+import os
 
 import torch
+from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import transforms
 import lmdb
@@ -10,59 +14,99 @@ from tqdm import tqdm
 from dataset import ImageFileDataset, CodeRow
 from vqvae import VQVAE
 
+from GANsynth_pytorch.pytorch_nsynth_lib.nsynth import (
+    NSynth, WavToSpectrogramDataLoader)
+
+HOP_LENGTH = 512
+N_FFT = 2048
+FS_HZ = 16000
+
 
 def extract(lmdb_env, loader, model, device):
     index = 0
 
     with lmdb_env.begin(write=True) as txn:
-        pbar = tqdm(loader)
+        pbar_loader = tqdm(loader)
 
-        for img, _, filename in pbar:
-            img = img.to(device)
+        for sample_batch, sample_name_batch, pitch_batch in pbar_loader:
+            sample_batch = sample_batch.to(device)
 
-            _, _, _, id_t, id_b = model.encode(img)
+            *_, id_t, id_b = model(sample_batch)
             id_t = id_t.detach().cpu().numpy()
             id_b = id_b.detach().cpu().numpy()
 
-            for file, top, bottom in zip(filename, id_t, id_b):
+            for file, top, bottom in zip(sample_name_batch, id_t, id_b):
                 row = CodeRow(top=top, bottom=bottom, filename=file)
                 txn.put(str(index).encode('utf-8'), pickle.dumps(row))
                 index += 1
-                pbar.set_description(f'inserted: {index}')
+                pbar_loader.set_description(f'inserted: {index}')
 
         txn.put('length'.encode('utf-8'), str(index).encode('utf-8'))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--main_output_dir', type=str, required=True)
     parser.add_argument('--size', type=int, default=256)
-    parser.add_argument('--ckpt', type=str)
-    parser.add_argument('--name', type=str)
-    parser.add_argument('path', type=str)
+    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--num_workers', type=int, default=4,
+                        help='Number of worker processes for the Dataloaders')
+    parser.add_argument('--dataset_path', type=str, required=True)
+    parser.add_argument('--model_weights_path', type=str, required=True)
+    parser.add_argument('--model_parameters_path', type=str, required=True)
+    parser.add_argument('--device', type=str, default='cpu',
+                        choices=['cpu', 'cuda'])
 
     args = parser.parse_args()
 
-    device = 'cuda'
+    MAIN_DIR = pathlib.Path(args.main_output_dir)
 
-    transform = transforms.Compose(
-        [
-            transforms.Resize(args.size),
-            transforms.CenterCrop(args.size),
-            transforms.ToTensor(),
-            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
-        ]
-    )
+    VQVAE_MODEL_WEIGHTS_PATH = pathlib.Path(args.model_weights_path)
+    # folder containing the vqvae weights is the ID
+    vqvae_id = VQVAE_MODEL_WEIGHTS_PATH.parts[-2]
+    vqvae_model_filename = VQVAE_MODEL_WEIGHTS_PATH.stem
 
-    dataset = ImageFileDataset(args.path, transform=transform)
-    loader = DataLoader(dataset, batch_size=128, shuffle=False, num_workers=4)
+    OUTPUT_DIR = MAIN_DIR / f'vqvae-{vqvae_id}-weights-{vqvae_model_filename}/'
+    os.makedirs(OUTPUT_DIR, exist_ok=False)
 
-    model = VQVAE()
-    model.load_state_dict(torch.load(args.ckpt))
+    # store command-line parameters
+    with open(OUTPUT_DIR / 'command_line_parameters.json', 'w') as f:
+        json.dump(args.__dict__, f)
+
+    device = args.device
+
+    valid_pitch_range = [24, 84]
+
+    nsynth_dataset_with_samples_names = NSynth(
+        root=str(args.dataset_path),
+        valid_pitch_range=valid_pitch_range,
+        categorical_field_list=['note_str'],
+        squeeze_mono_channel=True)
+
+    # converts wavforms to spectrograms on-the-fly on GPU
+    loader = WavToSpectrogramDataLoader(
+        nsynth_dataset_with_samples_names,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers, shuffle=True,
+        pin_memory=True,
+        device=device, n_fft=N_FFT, hop_length=HOP_LENGTH)
+
+    in_channel = 2
+
+    with open(args.model_parameters_path, 'r') as f:
+        vqvae_parameters = json.load(f)
+
+    vqvae = VQVAE(**vqvae_parameters)
+    vqvae.load_state_dict(torch.load(args.model_weights_path,
+                                     map_location=device))
+    model = nn.DataParallel(vqvae)
     model = model.to(device)
     model.eval()
 
+    # TODO(theis): compute appropriate size for the map
     map_size = 100 * 1024 * 1024 * 1024
 
-    env = lmdb.open(args.name, map_size=map_size)
+    env = lmdb.open(str(OUTPUT_DIR.absolute()), map_size=map_size)
 
-    extract(env, loader, model, device)
+    with torch.no_grad():
+        extract(env, loader, model, device)
--- sample.py
+++ sample.py
@@ -1,33 +1,87 @@
+from typing import Union, Optional, Iterable
 import argparse
 import os
+from datetime import datetime
+import uuid
+import soundfile
+from tqdm import tqdm
+import numpy as np
 
 import torch
 from torchvision.utils import save_image
-from tqdm import tqdm
 
-from vqvae import VQVAE
+from vqvae import VQVAE, InferenceVQVAE
 from pixelsnail import PixelSNAIL
+from GANsynth_pytorch.pytorch_nsynth_lib.nsynth import (
+    wavfile_to_melspec_and_IF)
 
 
 @torch.no_grad()
-def sample_model(model, device, batch, size, temperature, condition=None):
-    row = torch.zeros(batch, *size, dtype=torch.int64).to(device)
+def sample_model(model: PixelSNAIL, device: Union[torch.device, str],
+                 batch_size: int, codemap_size: Iterable[int],
+                 temperature: float, condition: Optional[torch.Tensor] = None,
+                 constraint: Optional[torch.Tensor] = None):
+    """Generate a sample from the provided PixelSNAIL
+
+    Arguments:
+        model (PixelSNAIL)
+        device (torch.device or str):
+            The device on which to perform the sampling
+        batch_size (int)
+        codemap_size (Iterable[int]):
+            The size of the codemap to generate
+        temperature (float):
+            Sampling temperature (lower means the model is more conservative)
+        condition (torch.Tensor, optional, default None):
+            Another codemap to use as hierarchical conditionning for sampling.
+            If not provided, sampling is unconditionned.
+        constraint_2D (torch.Tensor, optional, default None):
+            If provided, fixes the top-left part of the generated 2D codemap
+            to be the given Tensor.
+            `constraint_2D.size` should be less or equal to codemap_size.
+    """
+    codemap = (torch.zeros(batch_size, *codemap_size, dtype=torch.int64)
+               .to(device)
+               )
+
+    constraint_height = -1
+    constraint_width = -1
+    if constraint is not None:
+        if list(constraint.shape) > codemap_size:
+            raise ValueError("Incorrect size of constraint, constraint "
+                             "should be smaller than the target codemap size")
+        else:
+            _, constraint_height, constraint_width = constraint.shape
+
+            padding_left = padding_top = 0
+            padding_bottom = codemap_size[0] - constraint_height
+            padding_right = codemap_size[1] - constraint_width
+            padder = torch.nn.ConstantPad2d(
+                (padding_left, padding_right, padding_top, padding_bottom),
+                value=0).to(device)
+            channel_dim = 1
+            codemap = (padder(constraint.unsqueeze(channel_dim))
+                       .detach()
+                       .squeeze(channel_dim)
+                       )
+
     cache = {}
 
-    for i in tqdm(range(size[0])):
-        for j in range(size[1]):
-            out, cache = model(row[:, : i + 1, :], condition=condition, cache=cache)
+    for i in tqdm(range(codemap_size[0])):
+        start_column = (0 if i > constraint_height
+                        else constraint_width + 1)
+        for j in range(start_column, codemap_size[1]):
+            out, cache = model(codemap[:, :i + 1, :], condition=condition,
+                               cache=cache)
             prob = torch.softmax(out[:, :, i, j] / temperature, 1)
             sample = torch.multinomial(prob, 1).squeeze(-1)
-            row[:, i, j] = sample
+            codemap[:, i, j] = sample
 
-    return row
+    return codemap
 
 
 def load_model(model, checkpoint, device):
     ckpt = torch.load(os.path.join('checkpoint', checkpoint))
-
-    
     if 'args' in ckpt:
         args = ckpt['args']
 
@@ -61,7 +115,7 @@ def load_model(model, checkpoint, device):
             n_cond_res_block=args.n_cond_res_block,
             cond_res_channel=args.n_res_channel,
         )
-        
+
     if 'model' in ckpt:
         ckpt = ckpt['model']
 
@@ -73,28 +127,116 @@ def load_model(model, checkpoint, device):
 
 
 if __name__ == '__main__':
-    device = 'cuda'
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch', type=int, default=8)
-    parser.add_argument('--vqvae', type=str)
-    parser.add_argument('--top', type=str)
-    parser.add_argument('--bottom', type=str)
-    parser.add_argument('--temp', type=float, default=1.0)
-    parser.add_argument('filename', type=str)
+    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--vqvae_parameters_path', type=str, required=True)
+    parser.add_argument('--vqvae_weights_path', type=str, required=True)
+    parser.add_argument('--pixelsnail_top_parameters_path', type=str,
+                        required=True)
+    parser.add_argument('--pixelsnail_top_weights_path', type=str,
+                        required=True)
+    parser.add_argument('--pixelsnail_bottom_parameters_path', type=str,
+                        required=True)
+    parser.add_argument('--pixelsnail_bottom_weights_path', type=str,
+                        required=True)
+    parser.add_argument('--temperature', type=float, default=1.0)
+    parser.add_argument('--hop_length', type=int, default=512)
+    parser.add_argument('--n_fft', type=int, default=2048)
+    parser.add_argument('--use_mel_frequency', type=int, default=True)
+    parser.add_argument('--sample_rate_hz', type=int, default=16000)
+    parser.add_argument('--condition_top_audio_path', type=str)
+    parser.add_argument('--constraint_top_audio_path', type=str)
+    parser.add_argument('--constraint_top_num_timesteps', type=int)
+    parser.add_argument('--output_directory', type=str, default='./')
 
     args = parser.parse_args()
 
-    model_vqvae = load_model('vqvae', args.vqvae, device)
-    model_top = load_model('pixelsnail_top', args.top, device)
-    model_bottom = load_model('pixelsnail_bottom', args.bottom, device)
+    run_ID = (datetime.now().strftime('%Y%m%d-%H%M%S-')
+              + str(uuid.uuid4())[:6])
+    print("Sample ID: ", run_ID)
 
-    top_sample = sample_model(model_top, device, args.batch, [32, 32], args.temp)
-    bottom_sample = sample_model(
-        model_bottom, device, args.batch, [64, 64], args.temp, condition=top_sample
-    )
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    decoded_sample = model_vqvae.decode_code(top_sample, bottom_sample)
-    decoded_sample = decoded_sample.clamp(-1, 1)
+    model_vqvae = VQVAE.from_parameters_and_weights(
+        args.vqvae_parameters_path,
+        args.vqvae_weights_path,
+        device=device
+        ).to(device).eval()
+    model_top = PixelSNAIL.from_parameters_and_weights(
+        args.pixelsnail_top_parameters_path,
+        args.pixelsnail_top_weights_path,
+        device=device
+        ).to(device).eval()
+    model_bottom = PixelSNAIL.from_parameters_and_weights(
+        args.pixelsnail_bottom_parameters_path,
+        args.pixelsnail_bottom_weights_path,
+        device=device
+        ).to(device).eval()
 
-    save_image(decoded_sample, args.filename, normalize=True, range=(-1, 1))
+    with torch.no_grad():
+        if args.condition_top_audio_path is not None:
+            condition_mel_spec_and_IF = wavfile_to_melspec_and_IF(
+                args.condition_top_audio_path)
+
+            (_, _, _, condition_code_top, condition_code_bottom,
+             *_) = model_vqvae.encode(condition_mel_spec_and_IF.to(device))
+
+            # repeat condition for the whole batch
+            top_code = condition_code_top.repeat(args.batch_size, 1, 1)
+        elif args.constraint_top_audio_path is not None:
+            constraint_mel_spec_and_IF = wavfile_to_melspec_and_IF(
+                args.constraint_top_audio_path)
+
+            (_, _, _, constraint_code_top, *_) = model_vqvae.encode(
+                constraint_mel_spec_and_IF.to(device))
+            constraint_code_top_restrained = (
+                constraint_code_top[:, :args.constraint_top_num_timesteps-1])
+            top_code_sample = sample_model(
+                model_top, device, batch_size=1, codemap_size=model_top.shape,
+                temperature=args.temperature,
+                constraint=constraint_code_top_restrained)
+
+            # repeat condition for the whole batch
+            top_code = top_code_sample.repeat(args.batch_size, 1, 1)
+        else:
+            top_code_sample = sample_model(
+                model_top, device, args.batch_size, model_top.shape,
+                args.temperature)
+            top_code = top_code_sample
+        bottom_sample = sample_model(
+            model_bottom, device, args.batch_size, model_bottom.shape,
+            args.temperature, condition=top_code
+        )
+
+        decoded_sample = model_vqvae.decode_code(top_code, bottom_sample)
+
+    inference_vqvae = InferenceVQVAE(model_vqvae, device,
+                                     hop_length=args.hop_length,
+                                     n_fft=args.n_fft)
+
+    def make_audio(mag_and_IF_batch: torch.Tensor) -> np.ndarray:
+        audio_batch = inference_vqvae.mag_and_IF_to_audio(
+            mag_and_IF_batch, use_mel_frequency=args.use_mel_frequency)
+        audio_mono_concatenated = audio_batch.flatten().cpu().numpy()
+        return audio_mono_concatenated
+
+    os.makedirs(args.output_directory, exist_ok=True)
+
+    audio_sample_path = os.path.join(args.output_directory, f'{run_ID}.wav')
+    soundfile.write(audio_sample_path, make_audio(decoded_sample),
+                    samplerate=args.sample_rate_hz)
+
+    # write spectrogram and IF
+    channel_dim = 1
+    for channel_index, channel_name in enumerate(
+            ['spectrogram', 'instantaneous_frequency']):
+        channel = decoded_sample.select(channel_dim, channel_index
+                                        ).unsqueeze(channel_dim)
+        save_image(
+            channel,
+            os.path.join(args.output_directory, f'{run_ID}-{channel_name}.png'),
+            nrow=args.batch_size,
+            # normalize=True,
+            # range=(-1, 1),
+            # scale_each=True,
+        )
