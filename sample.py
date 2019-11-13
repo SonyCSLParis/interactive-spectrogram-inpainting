9725c04cbaa2290bcbf8485917a93c6eae9b2f83
Theis Bazin <theis.bazin@outlook.com>
2019-11-13 15:38:55 +0100

    :fire: Remove dead load_model function

--- sample.py
+++ sample.py
@@ -80,52 +80,6 @@ def sample_model(model: PixelSNAIL, device: Union[torch.device, str],
     return codemap
 
 
-def load_model(model, checkpoint, device):
-    ckpt = torch.load(os.path.join('checkpoint', checkpoint))
-    if 'args' in ckpt:
-        args = ckpt['args']
-
-    if model == 'vqvae':
-        model = VQVAE()
-
-    elif model == 'pixelsnail_top':
-        model = PixelSNAIL(
-            [32, 32],
-            512,
-            args.channel,
-            5,
-            4,
-            args.n_res_block,
-            args.n_res_channel,
-            dropout=args.dropout,
-            n_out_res_block=args.n_out_res_block,
-        )
-
-    elif model == 'pixelsnail_bottom':
-        model = PixelSNAIL(
-            [64, 64],
-            512,
-            args.channel,
-            5,
-            4,
-            args.n_res_block,
-            args.n_res_channel,
-            attention=False,
-            dropout=args.dropout,
-            n_cond_res_block=args.n_cond_res_block,
-            cond_res_channel=args.n_res_channel,
-        )
-
-    if 'model' in ckpt:
-        ckpt = ckpt['model']
-
-    model.load_state_dict(ckpt)
-    model = model.to(device)
-    model.eval()
-
-    return model
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch_size', type=int, default=8)
