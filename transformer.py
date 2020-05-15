612092830264caf38d10eaa982c4ef2bb724678b
Theis Bazin <theis.bazin@outlook.com>
2020-05-15 14:25:36 +0200

    :zap: Always cache memory thanks to AC attention
    The anti-causal attention in the Top Transformer
    ensures that the cached memory is never invalidated
    accross autoregressive sampling steps.

--- transformer.py
+++ transformer.py
@@ -1011,10 +1011,10 @@ class VQNSynthTransformer(nn.Module):
             if self.use_identity_memory_mask:
                 memory_mask = self.identity_memory_mask
             if memory is None:
+                src_mask = None
+                if self.self_conditional_model:
                 anti_causal_mask = self.causal_mask.t()
-                src_mask = (None if not self.self_conditional_model
-                            else anti_causal_mask
-                            )
+                    src_mask = anti_causal_mask
                 memory = self.transformer.encoder(
                     time_major_source_sequence,
                     mask=src_mask)
